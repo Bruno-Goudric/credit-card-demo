91d1b8f5eb76ee41f8f3b152bcdf7629cbd9d9fd
Bruno Ferreira dos Santos Silva <brunolfsports@hotmail.com>
2020-04-16 17:30:56 -0300

    add gift animate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards';
 import './App.css';
+import 'react-credit-cards/es/styles-compiled.css'
+
 
 function App() {
-  return (
+  const [number, setNumber] = useState('')
+  const [name, setName] = useState('')
+  const [expiry, setExpiry] = useState('')
+  const [cvc, setCvc] = useState('')
+  const [focus, setFocus] = useState('')
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+      <section>
+          <Cards
+            number={number}
+            name={name}
+            expiry={expiry}
+            cvc={cvc}
+            focused={focus}
+          />
+        </section>
+        <form>
+        <h1>Payment Details</h1>
+        <input 
+          type="text" 
+          name='name' 
+          placeholder='Name' 
+          value={name} 
+          onChange={e => setName(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <input 
+          type="tel" 
+          name='number' 
+          placeholder='Card Number' 
+          value={number} 
+          onChange={e => setNumber(e.target.value)}
+          onFocus={e => setFocus(e.target.name)}
+        />
+        <div className="input-group">
+          <input 
+            type="text" 
+            name='expiry' 
+            placeholder='MM/YY' 
+            value={expiry} 
+            onChange={e => setExpiry(e.target.value)}
+            onFocus={e => setFocus(e.target.name)}
+          />
+          <input 
+            type="tel"
+            name='cvc' 
+            placeholder='CVC' 
+            value={cvc} 
+            onChange={e => setCvc(e.target.value)}
+            onFocus={e => setFocus(e.target.name)}
+          />
+        </div>
+        <button className="button" type="submit">Pay</button>
+      </form>
+      </div>
     </div>
   );
 }
